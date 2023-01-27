fa065d187f9726141f0c6468ae81d42b02f28648
shazilmemon <shazilmemon6@gmail.com>
2023-01-27 03:57:05 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,120 @@
+// var s = 0;
+// while(s<10){
+//     console.log(s);
+//     s++;
+// }
+// function clickbtn() {
+//     alert("click")
+// }
+
+// function getname() {
+//     var name = document.getElementById("name");
+//     alert(name.value)
+// }
+
+
+// function setname() {
+//  var name = document.getElementById('name');
+ 
+//  name.value = "pakistan navy";
+//  var para = document.getElementById("para");
+
+//  para.innerHTML = "hello shazil ahmed welcome to pakistan navy"
+// }
+
+// setname();
+
+// EDIUS PRO. ...
+// Autodesk Smoke. ...
+// Avid Media Composer. ...
+// Final Cut Pro. ...
+// Hitfilm Pro. ...
+// Lightworks. ...
+// Blender. ...
+
+
+
+// function getnumber(num){
+//     var result1 = document.getElementById("result1");
+
+//     result1.value += num;
+// }
+
+// function clearresult() {
+    
+//     var result1 = document.getElementById("result1");
+
+//     result1.value = ""
+
+// }
+
+// function getresult() {
+//     var result1 = document.getElementById("result1");
+
+//     result1.value = eval(result1.value)
+// }
+// function readmore() {
+//     var text = ("Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui mollitia incidunt, dignissimos commodi vitae minus sed? Reprehenderit, laborum blanditiis! Provident fuga, maxime at nesciunt accusamus ipsum ab iste cum et!")
+//     var paragraph = document.getElementById("pera");
+//     paragraph. innerHTML = text 
+// }
+
+// function readmore(params) {
+//   var text = ("adipisicing elit. Totam dolorem, sapiente minima voluptates ratione at ex molestiae laudantium in. Earum ad cumque repellendus! Magni, ad temporibus! Quidem impedit quod ad") 
+//   var paragraph = document.getElementById("pera");
+//   paragraph. innerHTML = text
+// }
+
+
+// function big() {
+//   var img = document.getElementById("car")  
+//   img.className = 'hidden'
+// }
+function changeimage() {
+  
+    var img = document.getElementById("image")
+     img.src ='https://tse1.mm.bing.net/th?id=OIP.O5xBzo-7LYZuOp-U4htFcQHaFw&pid=Api&P=0'
+}
+
+function changeimagebefore() {
+    var img = document.getElementById("image")
+    img.src = 'https://tse2.mm.bing.net/th?id=OIP.9hpL-eBZRi_t3tgP6CdBXAHaE8&pid=Api&P=0'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
